5119eaeacf3822dd6e837eefbd78f6ebba6b8b84
Oleksandr Grytsov <al1img@gmail.com>
2017-11-21 19:52:38 +0200

    Add smartsocket build to example/apps
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>

---
+++ examples/apps/smartsocket/main.c
@@ -0,0 +1,51 @@
+#include <openthread/config.h>
+#include <openthread-core-config.h>
+#include <assert.h>
+
+#include <openthread/cli.h>
+#include <openthread/diag.h>
+#include <openthread/openthread.h>
+#include <openthread/platform/platform.h>
+#include <openthread/platform/logging.h>
+
+void otTaskletsSignalPending(otInstance *aInstance)
+{
+    (void)aInstance;
+}
+
+int main(int argc, char *argv[])
+{
+    otInstance *sInstance;
+
+    PlatformInit(argc, argv);
+
+    sInstance = otInstanceInitSingle();
+    assert(sInstance);
+
+    otCliUartInit(sInstance);
+
+    while (1)
+    {
+        otTaskletsProcess(sInstance);
+        PlatformProcessDrivers(sInstance);
+    }
+
+    return 0;
+}
+
+/*
+ * Provide, if required an "otPlatLog()" function
+ */
+#if OPENTHREAD_CONFIG_LOG_OUTPUT == OPENTHREAD_CONFIG_LOG_OUTPUT_APP
+void otPlatLog(otLogLevel aLogLevel, otLogRegion aLogRegion, const char *aFormat, ...)
+{
+    OT_UNUSED_VARIABLE(aLogLevel);
+    OT_UNUSED_VARIABLE(aLogRegion);
+    OT_UNUSED_VARIABLE(aFormat);
+
+    va_list ap;
+    va_start(ap, aFormat);
+    otCliPlatLogv(aLogLevel, aLogRegion, aFormat, ap);
+    va_end(ap);
+}
+#endif
